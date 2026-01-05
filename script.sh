ccd722840d47d6473db3b1f59d5a9279e6ef733b
Alex <alex@MacBook-Air-Alex.local>
2026-01-05 03:30:05 +0300

    added hostnames

--- script.sh
+++ script.sh
@@ -12,20 +12,24 @@ apt install ansible
 
 # data aggregation
 read -p "How many servers you want to create? " server_count
-echo "You want to create "$server_count" servers"
+#echo "You want to create "$server_count" servers"
 
 for (( i=1; i<=server_count; i++ )); do
-    ssh-keygen -t rsa -b 4096 -f "./dir_to_ignore/server_key_$i" -N ""
+    echo "Configuring server $i ..."
+    read -p "Enter your $i server hostname: " hostname
+    ssh-keygen -t rsa -b 4096 -f "./dir_to_ignore/$hostname" -N ""
 #    echo "Generated key pair for server $i: server_key_$i and server_key_$i.pub"
     
     read -p "Enter your $i server ip address: " ip_address
     read -sp "Enter your $i server password: " password && echo
+    echo "Check your $i server hostname: " $hostname
     echo "Check your $i server ip: " $ip_address
     echo "Check your $i server password: " $password
     echo "[server_$i]" >> ./dir_to_ignore/hosts.txt
-    echo "vm$i ansible_host=$ip_address ansible_user=root ansible_password=${password} ansible_connection=ssh" >> ./dir_to_ignore/hosts.txt
+    echo "$hostname ansible_host=$ip_address ansible_user=root ansible_password=${password} ansible_connection=ssh" >> ./dir_to_ignore/hosts.txt
     unset ip_address
     unset password
+    unset hostname
 done
 
 # DEBUG
@@ -36,4 +40,12 @@ cat ./dir_to_ignore/hosts.txt
 ansible-playbook -i ./dir_to_ignore/hosts.txt setup.yml
 
 # clear sensitive data
-echo " " > ./dir_to_ignore/hosts.txt
+rm -f ./dir_to_ignore/hosts.txt
+
+mkdir -p $HOME/.ssh/setup/
+mv ./dir_to_ignore/* $HOME/.ssh/setup/
+chmod 700 $HOME/.ssh/setup/
+chmod 600 $HOME/.ssh/setup/*
+rm -rf ./dir_to_ignore/
+
+grep -v "*.pub" /$HOME/.ssh/setup/
